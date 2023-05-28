57a0e3f714b3e3873f10948153f82283f1b81f39
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-28 11:32:28 -0300

    commit

--- uni1/simulacoes_cache/totalmente_associativo/32bytes.py
+++ uni1/simulacoes_cache/totalmente_associativo/32bytes.py
@@ -2,7 +2,7 @@ from cachesim import CacheSimulator, Cache, MainMemory
 
 mem_principal = MainMemory(65536)
 
-cache = Cache("L1",1024,32,1024,"LRU")
+cache = Cache("L1",1,32,1024,"LRU")
 mem_principal.load_to(cache)
 mem_principal.store_from(cache)
 
